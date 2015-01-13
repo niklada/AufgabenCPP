3c0dc6d9235619740f71da7327f9b95114e9bfcc
Niklas A <adams.niklas@web.de>
2015-01-13 13:29:02 +0100

    11 fertig

--- A11/a111.cpp
+++ A11/a111.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
 #include "myqueue.h"
+#include <string>
 using namespace std;
 int main(){
-  MyQueue<int> q={5,4};
-  //q.pop_front();
-  cout<<(q.empty()? "is empty" : "not empty")<<endl;
-  cout<<q.size()<<endl;
+  MyQueue<int> q={3,5,4};
+  cout<<"startqueue "<<q<<endl;
+  cout<<"popfront"<<endl;
+  q.pop_front();
+  cout<<q<<(q.empty()? "is empty" : "not empty")<<endl;
+  cout<<"Größe "<<q.size()<<endl;
+  cout<<"pushback 14"<<endl;
+  q.push_back(14);
   cout<<q<<endl;
   MyQueue<int> a(move(q));
-  cout<<(a.empty()? "is empty" : "not empty")<<endl;
+  cout<<"moved(constructor) to a: "<<(a.empty()? "is empty" : "not empty")<<endl;
   MyQueue<int> b=move(a);
-  cout<<(b.empty()? "is empty" : "not empty")<<endl;
+  cout<<"moved(assigned) to b: "<<(b.empty()? "is empty" : "not empty")<<endl;
+  cout<<"for each loop:"<<endl;
   for(auto elem : b){
     cout << elem <<endl;
   }
+  MyQueue<string> stringQ={"das","ist","die","infixdarstellung"};
+  cout<<"String queue"<<endl<<stringQ<<endl;
+  MyQueue<char> charQ={'(','4','+','5',')','*','3'};
+  cout<<"Character queue"<<endl<<charQ<<endl;
 }
--- A11/myqueue.h
+++ A11/myqueue.h
@@ -10,14 +10,31 @@ private:
   list<T> queue;
 public:
   MyQueue(MyQueue && q) : queue(move(q.queue)){};
-  MyQueue(initializer_list<T>);
-  MyQueue& operator =(MyQueue&& q)=default;
-  void push_back(T);
-  void pop_front();
-  T size();
-  bool empty();
-  typename list<T>::iterator begin();
-  typename list<T>::iterator end();
-  friend ostream& operator<<(ostream& o,const MyQueue& q);
+  MyQueue(initializer_list<T> lis):queue(lis){}
+  MyQueue& operator =(MyQueue<T>&& q)=default;
+  void push_back(T a){
+    queue.push_back(a);
+  }
+  void pop_front(){ 
+    queue.pop_front(); 
+  }
+  T size(){
+    return queue.size();
+  }
+  bool empty(){
+    return queue.empty();
+  }
+  typename list<T>::iterator begin(){
+    return queue.begin();
+  }
+  typename list<T>::iterator end(){
+    return queue.end();
+  }
+  friend ostream& operator<<(ostream& o, const MyQueue<T>& q){
+    for(auto iterator=q.queue.begin(), end=q.queue.end(); iterator!= end;++iterator){
+      o<<*iterator<<" ";
+    }
+    return o;
+  }
 };
 #endif
