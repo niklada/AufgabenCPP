660346099e64b1d123217a233c1bc41787b1de23
Niklas A <adams.niklas@web.de>
2015-03-12 14:48:12 +0100

    endProbeklausur

--- Probeklausur/ident.h
+++ Probeklausur/ident.h
@@ -1,5 +1,5 @@
 // Prüfung C++, Winter 2013/14, Terstegge/Voss
 
-const char * matse_name   = "Niklas Adams";
-const char * matse_matrnr = "4005934";
+const char * matse_name   = "";
+const char * matse_matrnr = "";
 
